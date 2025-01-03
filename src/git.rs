f6d1b2c8b6f86ba075fcb503ecc732fb3d856e61
Meowu <474384902@qq.com>
2025-01-03 21:29:31 +0800

    feat: impl git clone.

--- src/git.rs
+++ src/git.rs
@@ -1,6 +1,17 @@
 use anyhow::{anyhow, Ok, Result};
+use git2::{self, build::RepoBuilder, Repository};
 use reqwest::Client;
+use std::path;
 use tokio::process::Command;
+
+pub fn open_repository(path: &str) -> Result<Repository, git2::Error> {
+    Repository::open(path)
+}
+
+pub async fn clone_repo(url: &str, path_str: &str) -> Result<Repository> {
+    let repo = RepoBuilder::new().clone(url, path::Path::new(path_str))?;
+    Ok(repo)
+}
 
 pub async fn git_exists(url: &str) -> Result<bool> {
     let output = Command::new("git")
@@ -14,11 +25,13 @@ pub async fn git_exists(url: &str) -> Result<bool> {
     } else {
         let err_message = String::from_utf8_lossy(&output.stderr);
 
-        if (err_message.contains("not found") || err_message.contains("does not exist")) {
+        if err_message.contains("not found") || err_message.contains("does not exist") {
+            println!("Not found repo.");
             return Ok(false);
         }
 
         // such as auth err.
+        println!("ERR code: {}", err_message);
         Err(anyhow!("Failed to check git URL: {}", err_message))
     }
 }
@@ -29,9 +42,13 @@ pub async fn is_valid_git_url(url: &str) -> Result<bool> {
 
     match response.status() {
         reqwest::StatusCode::OK => Ok(true),
-        reqwest::StatusCode::NOT_FOUND => Ok(false),
+        reqwest::StatusCode::NOT_FOUND => {
+            println!("Repo not found.");
+            Ok(false)
+        }
         _ => {
             let err_message = format!("Unexpected status code: {}", response.status());
+            println!("ERR code: {}", err_message);
             Err(anyhow!("Failed to check git URL: {}", err_message))
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use anyhow::{anyhow, Result};
-use git::is_valid_git_url;
+use git::{git_exists, is_valid_git_url};
 
 mod git;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let url = "https://github.com/tokio-rs/axum";
-    let valid = is_valid_git_url(url).await.is_err_and(|e| {
-        println!("invalid --> {:?}", e);
-        false
-    });
+    let url = "https://github.com/zasper-io/zasper";
+    let valid = is_valid_git_url(url).await?;
     if valid {
         println!("Git exists.");
     } else {
         println!("Invalid git url.");
     }
+
+    let repo = git::clone_repo(url, "repos/zion").await?;
+    println!("Successfully cloned repository to: {:?}", repo.path());
     Ok(())
 }
