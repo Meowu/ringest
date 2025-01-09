0dbaa6248be95fe3790e3f6161c47a82aed0c5c4
Meowu <474384902@qq.com>
2025-01-09 18:01:11 +0800

    add prompts.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
 use std::path::{Path, PathBuf};
 use std::env;
+use std::fs;
 use anyhow::{anyhow, Result};
 use git::{git_exists, is_valid_git_url};
 
 mod file;
 mod git;
 mod llm;
+mod prompts;
+
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let url = "https://github.com/zasper-io/zasper";
-    let bore = "https://github.com/ekzhang/bore.git";
-    let valid = is_valid_git_url(url).await?;
-    if valid {
-        println!("Git repo exists.");
-    } else {
-        println!("Invalid git url.");
-    }
 
-    let repo_name = Path::new(bore)
+    let string_to_path = |seg: &str| { format!("repos/{}", seg) };
+
+    let otter = "https://github.com/tchayen/red-otter.git";
+
+    // let valid = is_valid_git_url(url).await?;
+    // if valid {
+    //     println!("Git repo exists.");
+    // } else {
+    //     println!("Invalid git url.");
+    // }
+
+    let repo_name = Path::new(otter)
         .file_stem()
         .ok_or(anyhow!("Invalid repo url format"))?
         .to_str()
         .ok_or(anyhow!("Invalid repo url format"))?;
-    let clone_dest = format!("repos/{}", repo_name);
+    let clone_dest = string_to_path(repo_name);
     println!("Ready to clone into: {}", clone_dest);
 
     // git::clone_repo(bore, &clone_dest).await?;
@@ -38,13 +44,25 @@ async fn main() -> Result<()> {
     // println!("Successfully written content to {}", output_path.display());
 
     let api_url = "https://api.openai.com/v1/chat/completions";
+    let file_name = "bore/llm.txt";
+    let dest = string_to_path(file_name);
+    let code = fs::read_to_string(&dest)?;
+    let lines: Vec<&str> = code.lines().collect();
+    let len = lines.iter().count();
+    println!("Total {} lines under {}", len, dest);
+
+
     let model = "gpt-4o";
-    let content = "What's the sky color?";
+    let content = prompts::prompts::RUST_PROMPT.replace("{{file_content}}", &code);
     let api_key = env::var("OPENAI_API_KEY").expect("API_KEY not exists");
 
     let llm_client = llm::OpenAIClient::new(api_url);
 
-    let answer = llm::send_to_llm(&llm_client, &api_key, model, content).await?;
-    println!("\n{}\nLLM Response:\n{}", content, answer);
+    println!("Prompting...");
+    let answer = llm::send_to_llm(&llm_client, &api_key, model, &content).await?;
+    let output = format!("LLM Response:\n{}", answer);
+    fs::write(string_to_path("bore/llm_output.md"), output)?;
+    println!("Done!");
+
     Ok(())
 }
