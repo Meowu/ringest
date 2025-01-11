fc9ab56c2c37101853e241a6df22e83c55359c0e
Meowu <474384902@qq.com>
2025-01-11 19:29:55 +0800

    feat: improve code to avoid duplicate clone or write.

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn main() -> Result<()> {
     //     println!("Invalid git url.");
     // }
 
-    let repo = pratt;
+    let repo = otter;
     let repo_name = Path::new(repo)
         .file_stem()
         .ok_or(anyhow!("Invalid repo url format"))?
@@ -30,24 +30,35 @@ async fn main() -> Result<()> {
         .ok_or(anyhow!("Invalid repo url format"))?;
     let clone_dest = string_to_path(repo_name);
 
-    println!("Ready to clone into: {}", clone_dest);
-
-    git::clone_repo(repo, &clone_dest).await?;
-    println!("Successfully cloned repository to: {:?}", clone_dest);
+    if !Path::new(&clone_dest).exists() {
+        println!("Ready to clone into: {}", clone_dest);
+        git::clone_repo(repo, &clone_dest).await?;
+        println!("Successfully cloned repository to: {:?}", clone_dest);
+    } else {
+        println!("Repo exists, ready to concat files.");
+    }
 
     // concat files and write to local.
-    let code_text = file::read_and_concat_files(&clone_dest, "src").await?;
-    let mut output_txt = PathBuf::from(&clone_dest);
-    output_txt.push("llm.txt");
-    fs::write(&output_txt, &code_text)?;
+    let mut code_path = PathBuf::from(&clone_dest);
+    code_path.push("llm.txt");
+
+    let mut code_text = String::new();
+    if !code_path.exists() {
+        let code = file::read_and_concat_files(&clone_dest, "src").await?;
+        fs::write(&code_path, &code)?;
+        code_text.push_str(&code);
+    } else {
+        let code = fs::read_to_string(&code_path)?;
+        code_text.push_str(&code);
+    }
 
     let lines: Vec<&str> = code_text.lines().collect();
     let len = lines.iter().count();
-    println!("Successfully written concat content to {}, {} lines in total.", output_txt.display(), len);
+    println!("Successfully written concat content to {}, {} lines in total.", code_path.display(), len);
 
     let api_url = "https://api.openai.com/v1/chat/completions";
     let model = "gpt-4o";
-    let content = prompts::prompts::RUST_PROMPT.replace("{{file_content}}", &code_text);
+    let content = prompts::prompts::COMMON_PROMPT.replace("{{file_content}}", &code_text);
     let api_key = env::var("OPENAI_API_KEY").expect("API_KEY not exists");
 
     let llm_client = llm::OpenAIClient::new(api_url);
@@ -55,7 +66,7 @@ async fn main() -> Result<()> {
     println!("Prompting...");
     let answer = llm::send_to_llm(&llm_client, &api_key, model, &content).await?;
     let output = format!("LLM Response:\n{}", answer);
-    fs::write(format!("{}/llm_output.md", clone_dest), output)?;
+    fs::write(format!("{}/llm_output_common.md", clone_dest), output)?;
     println!("Done!");
 
     Ok(())
